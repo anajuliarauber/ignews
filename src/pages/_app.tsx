7372553eda880adf37ecbe5d2bea86338850ffe8
anajuliarauber <anajuliarauber@hotmail.com>
2022-09-03 15:24:14 -0300

    feat: implement _app page

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Header } from '../components/Header/index';
+import '../styles/global.scss';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
