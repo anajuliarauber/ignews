ecc367874533d16cf5825fdc35e07678b4ea65ad
anajuliarauber <anajuliarauber@hotmail.com>
2022-09-03 15:26:46 -0300

    feat: implement SubscribeButton component

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.scss';
+
+export function SubscribeButton() {
+  return (
+    <button type="button" className={styles.subscriptionButton}>
+      Subscribe now
+    </button>
+  );
+}
