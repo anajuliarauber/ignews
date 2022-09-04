6ecdec6ac41baac8901d982a3a5ae3fd9de86b60
anajuliarauber <anajuliarauber@hotmail.com>
2022-09-04 16:48:45 -0300

    feat: change GetServerSideProps to GetStaticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './home.module.scss';
 import Head from '../../node_modules/next/head';
 import { SubscribeButton } from '../components/SubscribeButton/index';
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import { stripe } from '../services/stripe';
 
 interface HomeProps {
@@ -37,7 +37,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1Le1O9KoIkHCmrxfJqEzodil');
 
   const product = {
@@ -51,5 +51,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, // 24 horas
   };
 };
