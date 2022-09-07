0516e0c75559c7f54b788962946d4139fc164314
anajuliarauber <anajuliarauber@hotmail.com>
2022-09-07 16:20:30 -0300

    feat: add next-auth authentication and save user on faunadb

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Header } from '../components/Header/index';
 import '../styles/global.scss';
+import { SessionProvider } from 'next-auth/react';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   );
 }
 
