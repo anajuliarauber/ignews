c46a2946c7b1303acc2c684aa1b16eaa161b2127
anajuliarauber <anajuliarauber@hotmail.com>
2022-11-05 16:52:28 -0300

    fix: stripe webhook secret key error

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -36,7 +36,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     let event: Stripe.Event;
 
     try {
-      event = stripe.webhooks.constructEvent(buf, secret, process.env.STRIPE_WEBHOOK_SECRET);
+      event = stripe.webhooks.constructEvent(buf, secret, process.env.STRIPE_WEB_HOOK_SECRET);
     } catch (err) {
       console.log(err);
       return res.status(400).send(`Webhook error: ${err.message}`);
